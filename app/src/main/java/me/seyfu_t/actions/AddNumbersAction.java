7ba4578bda788f626c486722a129f2d37863a432
seyfu-t <dev+git@seyfu-t.me>
2024-10-24 18:33:44 +0200

    implemented add_numbers action in AddNumbersAction.java

---
+++ app/src/main/java/me/seyfu_t/actions/AddNumbersAction.java
@@ -0,0 +1,20 @@
+package me.seyfu_t.actions;
+
+import java.util.AbstractMap;
+import java.util.Map.Entry;
+
+import com.google.gson.JsonObject;
+
+import me.seyfu_t.model.Action;
+
+public class AddNumbersAction implements Action{
+
+    @Override
+    public Entry<String, Object> execute(JsonObject arguments) {
+        int a = arguments.get("number1").getAsInt();
+        int b = arguments.get("number2").getAsInt();
+
+        return new AbstractMap.SimpleEntry<>("sum",a+b);
+    }
+    
+}
