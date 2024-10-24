abc1b20aedc20d2a977932fa6d06150fe377c268
seyfu-t <dev+git@seyfu-t.me>
2024-10-24 18:32:05 +0200

    created action abtraction as Action interface, consisting of a single execute method

---
+++ app/src/main/java/me/seyfu_t/model/Action.java
@@ -0,0 +1,13 @@
+package me.seyfu_t.model;
+
+import java.util.Map.Entry;
+
+import com.google.gson.JsonObject;
+
+public interface Action {
+    /*
+     *  @param takes in the contents of "arguments":{}
+     *  @return action result which is a key-value pair with the key being the name of the output (e.g. "sum" or "block") and the value being the result data itself
+     */
+    public Entry<String,Object> execute(JsonObject arguments);
+}
