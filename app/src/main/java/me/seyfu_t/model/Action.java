6604c09919d1aae620fb59c963a1ce3e8f508a57
seyfu-t <git@seyfu-t.me>
2024-10-27 03:00:59 +0100

    reformatted all files to be consistent

--- app/src/main/java/me/seyfu_t/model/Action.java
+++ app/src/main/java/me/seyfu_t/model/Action.java
@@ -6,8 +6,11 @@ import com.google.gson.JsonObject;
 
 public interface Action {
     /*
-     *  @param takes in the contents of "arguments":{}
-     *  @return action result which is a key-value pair with the key being the name of the output (e.g. "sum" or "block") and the value being the result data itself
+     * @param takes in the contents of "arguments":{}
+     * 
+     * @return action result which is a key-value pair with the key being the name
+     * of the output (e.g. "sum" or "block") and the value being the result data
+     * itself
      */
-    public Entry<String,Object> execute(JsonObject arguments);
+    public Entry<String, Object> execute(JsonObject arguments);
 }
