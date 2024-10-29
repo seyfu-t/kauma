08319fe043fc8f03ce11de4a36e44a98d78651fe
seyfu-t <seyfullahtalay@hotmail.com>
2024-10-30 00:48:55 +0100

    changed log statements for test console output, to be more readable

--- app/src/test/java/me/seyfu_t/AppTest.java
+++ app/src/test/java/me/seyfu_t/AppTest.java
@@ -1,5 +1,6 @@
 package me.seyfu_t;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.Assertions;
@@ -17,22 +18,19 @@ class AppTest {
     private static final String POLY2BLOCK_OUTPUT = "Poly2BlockOutput.json";
 
     @Test
-    @SuppressWarnings("LoggerStringConcat")
     void testAddSubtractActions() {
         String addSubtractInputPath = getClass().getClassLoader().getResource(ADD_SUBTRACT_INPUT).getFile();
         String addSubtractOutputPath = getClass().getClassLoader().getResource(ADD_SUBTRACT_OUTPUT).getFile();
 
         JsonObject inputJson = App.parseFilePathToJson(addSubtractInputPath);
-        log.info("Input JSON: " + inputJson.toString());
+        info("Input JSON: {0}", inputJson.toString());
 
-        // Execute your method(s) using the input JSON
         JsonObject actualOutputJson = App.getResponseJsonFromInputJson(inputJson);
+        info("Output JSON: {0}", actualOutputJson.toString());
 
-        // Read expected output JSON file
         JsonObject expectedOutputJson = App.parseFilePathToJson(addSubtractOutputPath);
-        log.info("Expected Output JSON: " + expectedOutputJson.toString());
+        info("Expected Output JSON: {0}", expectedOutputJson.toString());
 
-        // Compare actual output with expected output
         Assertions.assertEquals(expectedOutputJson, actualOutputJson,
                 "The output JSON does not match the expected output.");
     }
@@ -43,15 +41,19 @@ class AppTest {
         String poly2BlockOutputPath = getClass().getClassLoader().getResource(POLY2BLOCK_OUTPUT).getFile();
 
         JsonObject inputJson = App.parseFilePathToJson(poly2BlockInputPath);
-        log.info("Input JSON: " + inputJson.toString());
+        info("Input JSON: {0}", inputJson.toString());
 
         JsonObject actualOutputJson = App.getResponseJsonFromInputJson(inputJson);
-        log.info("Output JSON: " + actualOutputJson.toString());
+        info("Output JSON: {0}", actualOutputJson.toString());
 
         JsonObject expectedOutputJson = App.parseFilePathToJson(poly2BlockOutputPath);
-        log.info("Expected Output JSON: " + expectedOutputJson.toString());
+        info("Expected Output JSON: {0}", expectedOutputJson.toString());
 
         Assertions.assertEquals(expectedOutputJson, actualOutputJson,
                 "The output JSON does not match the expected output.");
+    }
+
+    private void info(String msg, Object...param){
+        log.log(Level.INFO, msg, param);
     }
 }
