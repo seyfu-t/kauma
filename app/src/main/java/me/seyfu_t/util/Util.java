5ef3f86e100711c953ad67615ca0bb904011eee5
seyfu-t <seyfullahtalay@hotmail.com>
2024-10-30 22:54:12 +0100

    added cut/pad method to 16 byte size of big ints in little endian with sign byte handling

--- app/src/main/java/me/seyfu_t/util/Util.java
+++ app/src/main/java/me/seyfu_t/util/Util.java
@@ -1,6 +1,7 @@
 package me.seyfu_t.util;
 
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class Util {
 
@@ -12,8 +13,26 @@ public class Util {
         for (int i = 0; i < oldBytes.length; i++) {
             newBytes[i] = oldBytes[oldBytes.length - i - 1];
         }
-
         return new BigInteger(1, newBytes);
     }
 
+    // Method for little-endian cutting/padding to 16 bytes
+    public static BigInteger littleEndianSignedBigIntTo16Bytes(BigInteger bigInt) {
+        byte[] byteArray = bigInt.toByteArray();
+        byte[] limitedByteArray = new byte[16];
+
+        // Handle sign-byte
+        if(byteArray[0] == 0){
+            byteArray = Arrays.copyOfRange(byteArray, 1, byteArray.length);
+        }
+        for (int i = 0; i<16;i++){
+            if(i < byteArray.length){
+                limitedByteArray[i] = byteArray[i];
+            }else{
+                limitedByteArray[i] = 0;
+            }
+        }
+        return new BigInteger(limitedByteArray);
+    }
+    
 }
