900c6bc876a34b6d6356e0acd519efce6a0bda72
seyfu-t <dev+git@seyfu-t.me>
2024-10-30 12:46:08 +0100

    implemented test block2poly

--- app/src/test/java/me/seyfu_t/AppTest.java
+++ app/src/test/java/me/seyfu_t/AppTest.java
@@ -16,6 +16,8 @@ class AppTest {
     private static final String ADD_SUBTRACT_OUTPUT = "AddSubtractNumbersOutput.json";
     private static final String POLY2BLOCK_INPUT = "Poly2BlockInput.json";
     private static final String POLY2BLOCK_OUTPUT = "Poly2BlockOutput.json";
+    private static final String BLOCK2POLY_INPUT = "Block2PolyInput.json";
+    private static final String BLOCK2POLY_OUTPUT = "Block2PolyOutput.json";
 
     @Test
     void testAddSubtractActions() {
@@ -47,6 +49,24 @@ class AppTest {
         info("Output JSON: {0}", actualOutputJson.toString());
 
         JsonObject expectedOutputJson = App.parseFilePathToJson(poly2BlockOutputPath);
+        info("Expected Output JSON: {0}", expectedOutputJson.toString());
+
+        Assertions.assertEquals(expectedOutputJson, actualOutputJson,
+                "The output JSON does not match the expected output.");
+    }
+
+    @Test
+    void testBlock2PolyXEXAction() {
+        String block2polyInputPath = getClass().getClassLoader().getResource(BLOCK2POLY_INPUT).getFile();
+        String block2polyOutputPath = getClass().getClassLoader().getResource(BLOCK2POLY_OUTPUT).getFile();
+
+        JsonObject inputJson = App.parseFilePathToJson(block2polyInputPath);
+        info("Input JSON: {0}", inputJson.toString());
+
+        JsonObject actualOutputJson = App.getResponseJsonFromInputJson(inputJson);
+        info("Output JSON: {0}", actualOutputJson.toString());
+
+        JsonObject expectedOutputJson = App.parseFilePathToJson(block2polyOutputPath);
         info("Expected Output JSON: {0}", expectedOutputJson.toString());
 
         Assertions.assertEquals(expectedOutputJson, actualOutputJson,
