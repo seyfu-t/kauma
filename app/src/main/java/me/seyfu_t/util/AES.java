058ef1771a96bd9806bcafa4196bb436e1d23699
seyfu-t <git@seyfu-t.me>
2024-10-31 19:42:47 +0100

    simplified encrypt and decrypt methods, to work directly with byte arrays

--- app/src/main/java/me/seyfu_t/util/AES.java
+++ app/src/main/java/me/seyfu_t/util/AES.java
@@ -1,34 +1,29 @@
 package me.seyfu_t.util;
 
-import java.util.Base64;
-
 import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class AES {
 
     @SuppressWarnings("UseSpecificCatch")
-    public static String encrypt(String plainText, SecretKey key) {
+    public static byte[] encrypt(byte[] plainText, byte[] key) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
-            return Base64.getEncoder().encodeToString(encryptedBytes);
+            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key, "AES"));
+            return cipher.doFinal(plainText);
         } catch (Exception e) {
-            return "";
+            throw new RuntimeException("AES decryption failed", e);
         }
     }
 
     @SuppressWarnings("UseSpecificCatch")
-    public static String decrypt(String encryptedText, SecretKey key) {
+    public static byte[] decrypt(byte[] encryptedText, byte[] key) {
         try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
-            byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-            return new String(decryptedBytes);
+            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"));
+            return cipher.doFinal(encryptedText);
         } catch (Exception e) {
-            return "";
+            throw new RuntimeException("AES decryption failed", e);
         }
     }
 
