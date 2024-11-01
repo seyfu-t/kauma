caa98c422b825d7daf3ed0d228f22c224b813e0c
seyfu-t <git@seyfu-t.me>
2024-11-01 02:39:54 +0100

    implemented gcm for block2poly

--- app/src/main/java/me/seyfu_t/actions/Block2PolyAction.java
+++ app/src/main/java/me/seyfu_t/actions/Block2PolyAction.java
@@ -20,6 +20,8 @@ public class Block2PolyAction implements Action {
 
         if (semantic.equalsIgnoreCase("xex")) {
             coefficients = convertBlock2PolyXEX(base64Block);
+        } else if (semantic.equalsIgnoreCase("gcm")) {
+            coefficients = convertBlock2PolyGCM(base64Block);
         }
 
         return new AbstractMap.SimpleEntry<>("coefficients", coefficients);
@@ -35,6 +37,26 @@ public class Block2PolyAction implements Action {
             for (int bitIndex = 0; bitIndex < 8; bitIndex++) {
                 if ((blockByteArray[byteIndex] & (1 << bitIndex)) != 0) {
                     coefficients[slot] = (byteIndex * 8) + bitIndex;
+                    slot++;
+                }
+            }
+        }
+
+        coefficients = Arrays.copyOfRange(coefficients, 0, slot);
+        Arrays.sort(coefficients);
+        return coefficients;
+    }
+
+    private static int[] convertBlock2PolyGCM(String base64Block) {
+        byte[] blockByteArray = Base64.getDecoder().decode(base64Block);
+
+        int[] coefficients = new int[128];
+        int slot = 0;// to know the current index of the coefficients array
+        // check each bit and add coefficient if bit is set
+        for (int byteIndex = 0; byteIndex < 16; byteIndex++) {
+            for (int bitIndex = 0; bitIndex < 8; bitIndex++) {
+                if ((blockByteArray[byteIndex] & (1 << bitIndex)) != 0) {
+                    coefficients[slot] = (byteIndex * 8) + 7 - bitIndex;
                     slot++;
                 }
             }
