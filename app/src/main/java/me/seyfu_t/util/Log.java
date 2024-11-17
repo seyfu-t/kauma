aa6a2fc61839a7d47e02a2fd315f8670561b5104
seyfu-t <seyfullahtalay@hotmail.com>
2024-11-17 19:37:45 +0100

    fix: log level 'debug' should contain 'info' as well

--- app/src/main/java/me/seyfu_t/util/Log.java
+++ app/src/main/java/me/seyfu_t/util/Log.java
@@ -7,17 +7,17 @@ import me.seyfu_t.App;
 public class Log {
 
     public static void debug(String msg, Object... args) {
-        if (App.LOG_LEVEL == Level.FINE)
+        if (App.LOG_LEVEL == Level.FINE || App.LOG_LEVEL == Level.INFO)
             System.out.println("[DEBUG] " + String.format(msg, args));
     }
 
     public static void debug(Object msg) {
-        if (App.LOG_LEVEL == Level.FINE)
+        if (App.LOG_LEVEL == Level.FINE || App.LOG_LEVEL == Level.INFO)
             System.out.println("[DEBUG] " + msg);
     }
 
     public static void debug(String prefix, Object msg) {
-        if (App.LOG_LEVEL == Level.FINE)
+        if (App.LOG_LEVEL == Level.FINE || App.LOG_LEVEL == Level.INFO)
             System.out.println("[DEBUG] " + prefix + " " + msg);
     }
 
