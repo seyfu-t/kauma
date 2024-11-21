c6bda833a51daec54d13f5778a7d6afabce22377
seyfu-t <dev+git@seyfu-t.me>
2024-11-21 15:02:04 +0100

    remove redundant check and fix wrong polarity in check

--- app/src/main/java/me/seyfu_t/model/GF128Poly.java
+++ app/src/main/java/me/seyfu_t/model/GF128Poly.java
@@ -69,7 +69,7 @@ public class GF128Poly {
             return true;
 
         for (int i = 0; i < this.coefficients.size(); i++) {
-            if (this.coefficients.get(i) != null && this.coefficients.get(i).isZero())
+            if (!this.coefficients.get(i).isZero())
                 return false;
         }
 
