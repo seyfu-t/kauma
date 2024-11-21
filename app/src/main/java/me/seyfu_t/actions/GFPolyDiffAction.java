4a859b4565366e1b791926e3d2c6a27b4e435108
seyfu-t <git@seyfu-t.me>
2024-11-22 00:52:16 +0100

    fix: handle edge case of empty poly results

--- app/src/main/java/me/seyfu_t/actions/GFPolyDiffAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFPolyDiffAction.java
@@ -7,6 +7,7 @@ import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
 import me.seyfu_t.model.GF128Poly;
+import me.seyfu_t.model.UBigInt16;
 import me.seyfu_t.util.Util;
 
 public class GFPolyDiffAction implements Action {
@@ -28,14 +29,21 @@ public class GFPolyDiffAction implements Action {
     public static GF128Poly gfPolyDiff(GF128Poly poly) {
         GF128Poly result = new GF128Poly();
 
-        if (poly.isZero())
+        if (poly.isZero()) {
+            result.setCoefficient(0, UBigInt16.Zero(true));
             return result;
+        }
 
         // Skip first one, effectively reduce all exponents by 1
         for (int i = 1; i < poly.size(); i++) {
             if (i % 2 == 1)
                 result.setCoefficient(i - 1, poly.getCoefficient(i));
         }
+
+        result = result.popLeadingZeros();
+
+        if (result.isZero())
+            result.setCoefficient(0, UBigInt16.Zero(true));
 
         return result.popLeadingZeros();
     }
