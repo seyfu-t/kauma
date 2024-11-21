bbfab834fa215f8f6d443ee8bcc12a84c2ede7dc
seyfu-t <git@seyfu-t.me>
2024-11-21 01:09:13 +0100

    extend gfpoly_pow to take larger exponents

--- app/src/main/java/me/seyfu_t/actions/GFPolyPowAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFPolyPowAction.java
@@ -15,7 +15,7 @@ public class GFPolyPowAction implements Action {
     @Override
     public Map<String, Object> execute(JsonObject arguments) {
         String[] poly = Util.convertJsonArrayToStringArray(arguments.get("A").getAsJsonArray());
-        int k = arguments.get("k").getAsInt();
+        UBigInt16 k = UBigInt16.fromBigInt(arguments.get("k").getAsBigInteger());
 
         GF128Poly a = new GF128Poly(poly);
 
@@ -61,6 +61,40 @@ public class GFPolyPowAction implements Action {
 
             // Divide power by 2
             pow >>= 1;
+        }
+
+        return result.popLeadingZeros();
+    }
+
+    public static GF128Poly gfPolyPow(GF128Poly poly, UBigInt16 pow) {
+        if (pow.isZero()) {
+            GF128Poly one = new GF128Poly();
+            one.setCoefficient(0, UBigInt16.Zero(true).setBit(0));
+            return one;
+        }
+
+        if (pow.sameAs(UBigInt16.Zero(true).setBit(0))) {
+            return poly.copy();
+        }
+
+        // Initialize result as 1 (identity element for multiplication)
+        GF128Poly result = new GF128Poly();
+        result.insertCoefficient(0, UBigInt16.Zero(true).setBit(0));
+
+        GF128Poly base = poly.copy();
+
+        // Binary exponentiation algorithm
+        while (!pow.isZero()) {
+            // If odd, multiply
+            if (pow.testBit(0)) {
+                result = GFPolyMulAction.gfPolyMul(result, base);
+            }
+
+            // Square
+            base = GFPolyMulAction.gfPolyMul(base, base);
+
+            // Divide power by 2
+            pow = pow.shiftRight(1);
         }
 
         return result.popLeadingZeros();
