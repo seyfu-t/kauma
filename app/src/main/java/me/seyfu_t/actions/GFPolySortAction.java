01de80141a7ec0969bd878eabe5520b157f7e484
seyfu-t <dev+git@seyfu-t.me>
2024-11-25 14:27:13 +0100

    swap out compare strategy to using the in GF128Poly class defined compare method

--- app/src/main/java/me/seyfu_t/actions/GFPolySortAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFPolySortAction.java
@@ -1,6 +1,7 @@
 package me.seyfu_t.actions;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,31 +46,33 @@ public class GFPolySortAction implements Action {
 
         // Java syntax...
         // lambda that in theory must be able to compare each possible pair p1 and p2
-        sortedList.sort((p1, p2) -> {
-            // Returning 1 means p1 > p2
-            // Returning 0 means p1 = p2
-            // Returning -1 means p1 < p2
+        // sortedList.sort((p1, p2) -> {
+        //     // Returning 1 means p1 > p2
+        //     // Returning 0 means p1 = p2
+        //     // Returning -1 means p1 < p2
 
-            // First compare by degree
-            int degreeComparison = Integer.compare(p1.degree(), p2.degree());
-            if (degreeComparison != 0)
-                return degreeComparison;
+        //     // First compare by degree
+        //     int degreeComparison = Integer.compare(p1.degree(), p2.degree());
+        //     if (degreeComparison != 0)
+        //         return degreeComparison;
 
-            // If both polynomials are empty (degree == -1)
-            if (p1.degree() == -1)
-                return 0;
+        //     // If both polynomials are empty (degree == -1)
+        //     if (p1.degree() == -1)
+        //         return 0;
 
-            // Compare coefficients from highest to lowest degree
-            for (int i = p1.degree(); i >= 0; i--) {
-                if (p1.getCoefficient(i).greaterThan(p2.getCoefficient(i))) {
-                    return 1;
-                } else if (p2.getCoefficient(i).greaterThan(p1.getCoefficient(i))) {
-                    return -1;
-                }
-            }
+        //     // Compare coefficients from highest to lowest degree
+        //     for (int i = p1.degree(); i >= 0; i--) {
+        //         if (p1.getCoefficient(i).greaterThan(p2.getCoefficient(i))) {
+        //             return 1;
+        //         } else if (p2.getCoefficient(i).greaterThan(p1.getCoefficient(i))) {
+        //             return -1;
+        //         }
+        //     }
 
-            return 0; // Polynomials are equal
-        });
+        //     return 0; // Polynomials are equal
+        // });
+
+        sortedList.sort(null);
 
         return sortedList;
     }
