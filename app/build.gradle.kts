472b392ac41c44983367cf7f99f9134c93ac1472
seyfu-t <seyfullahtalay@hotmail.com>
2024-11-30 21:52:13 +0100

    bump version up to 21 and add jmh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     application
+    id("me.champeau.jmh") version "0.7.2"
 }
 
 application {
@@ -32,7 +33,7 @@ testing {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
