207e01ecd3d60a61ec9476270c3f58b6e5f2f244
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-01 20:37:38 +0100

    added sff, ddf and edf back

--- app/src/main/java/me/seyfu_t/App.java
+++ app/src/main/java/me/seyfu_t/App.java
@@ -146,6 +146,9 @@ public class App {
             case "gfpoly_sort" -> new GFPolySortAction();
             case "gfpoly_diff" -> new GFPolyDiffAction();
             case "gfpoly_gcd" -> new GFPolyGCDAction();
+            case "gfpoly_factor_sff" -> new GFPolyFactorSFFAction();
+            case "gfpoly_factor_ddf" -> new GFPolyFactorDDFAction();
+            case "gfpoly_factor_edf" -> new GFPolyFactorEDFAction();
             default -> null;
         };
     }
