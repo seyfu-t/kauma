aae470a743f85df7b156c4546a842350e34ee82b
seyfu-t <git@seyfu-t.me>
2024-12-06 01:14:13 +0100

    implemented gfmul with FieldElementGCM, though wrong for now

--- app/src/main/java/me/seyfu_t/actions/GFMulAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFMulAction.java
@@ -3,6 +3,7 @@ package me.seyfu_t.actions;
 import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
+import me.seyfu_t.model.FieldElementGCM;
 import me.seyfu_t.model.UBigInt16;
 import me.seyfu_t.util.ResponseBuilder;
 
@@ -16,10 +17,46 @@ public class GFMulAction implements Action {
 
         boolean gcm = (semantic.equals("gcm"));
 
-        UBigInt16 a = UBigInt16.fromBase64(base64A, gcm);
-        UBigInt16 b = UBigInt16.fromBase64(base64B, gcm);
+        if (gcm) {
+            FieldElementGCM a = FieldElementGCM.fromBase64(base64A);
+            FieldElementGCM b = FieldElementGCM.fromBase64(base64B);
 
-        return ResponseBuilder.singleResponse("product", mulAndReduce(a, b).toBase64());
+            return ResponseBuilder.singleResponse("product", mulAndReduce(a, b).toBase64());
+        } else {
+            UBigInt16 a = UBigInt16.fromBase64(base64A);
+            UBigInt16 b = UBigInt16.fromBase64(base64B);
+
+            return ResponseBuilder.singleResponse("product", mulAndReduce(a, b).toBase64());
+        }
+
+    }
+
+    public static FieldElementGCM mulAndReduce(FieldElementGCM a, FieldElementGCM b) {
+        // Early zero checks
+        if (a.isZero() || b.isZero())
+            return FieldElementGCM.Zero();
+
+        FieldElementGCM result = FieldElementGCM.Zero();
+
+        // doing more than 128 rounds isn't possible
+        for (int i = 0; i < UBigInt16.BYTE_COUNT * Byte.SIZE; i++) {
+            if (b.testBit(0))
+                result = result.xor(a);
+
+            boolean overflow = a.testBit(127);
+            a = a.shiftLeft(1);
+
+            if (overflow)
+                a = a.xor(FieldElementGCM.REDUCTION_POLY);
+
+            b = b.shiftRight(1);
+
+            // Early termination if b becomes zero
+            if (b.isZero())
+                break;
+        }
+
+        return result;
     }
 
     public static UBigInt16 mulAndReduce(UBigInt16 a, UBigInt16 b) {
