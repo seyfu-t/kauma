497046d7309b5e703612bb3dabe49a005f6e0f84
seyfu-t <git@seyfu-t.me>
2024-12-07 12:19:39 +0100

    naive conversion of gcm_encrypt, gcm_decrypt and gcm_crack into using new FieldElement, though wrong right now

--- app/src/main/java/me/seyfu_t/actions/GCMEncryptAction.java
+++ app/src/main/java/me/seyfu_t/actions/GCMEncryptAction.java
@@ -8,8 +8,8 @@ import java.util.List;
 import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
+import me.seyfu_t.model.FieldElement;
 import me.seyfu_t.model.Tuple;
-import me.seyfu_t.model.UBigInt16;
 import me.seyfu_t.util.AES;
 import me.seyfu_t.util.ResponseBuilder;
 import me.seyfu_t.util.Util;
@@ -32,21 +32,21 @@ public class GCMEncryptAction implements Action {
     public static JsonObject gcmEncrypt(String algorithm, String base64Nonce, String base64Key,
             String base64Plaintext, String base64AD) {
 
-        UBigInt16 key = UBigInt16.fromBase64(base64Key, true);
+        FieldElement key = FieldElement.fromBase64GCM(base64Key);
         byte[] plaintextBytes = Base64.getDecoder().decode(base64Plaintext);
         byte[] ciphertextBytes = generateFullText(algorithm, base64Nonce, key, plaintextBytes);
         byte[] adBytes = Base64.getDecoder().decode(base64AD);
 
-        UBigInt16 H = calculateAuthKey(algorithm, key);
-        UBigInt16 L = calculateLengthOfADAndCiphertexts(adBytes, ciphertextBytes);
-        UBigInt16 ghashResult = ghash(H, adBytes, ciphertextBytes, L);
+        FieldElement H = calculateAuthKey(algorithm, key);
+        FieldElement L = calculateLengthOfADAndCiphertexts(adBytes, ciphertextBytes);
+        FieldElement ghashResult = ghash(H, adBytes, ciphertextBytes, L);
         // index -1 because 0 is the first ciphertext and this is one before that
-        UBigInt16 authTag = generateSingleTextBlock(AUTH_TAG_INDEX, algorithm, base64Nonce, key, ghashResult);
+        FieldElement authTag = generateSingleTextBlock(AUTH_TAG_INDEX, algorithm, base64Nonce, key, ghashResult);
 
         String ciphertext = Base64.getEncoder().encodeToString(ciphertextBytes);
-        String base64L = L.toBase64();
-        String base64H = H.toBase64();
-        String base64AuthTag = authTag.toBase64();
+        String base64L = L.toBase64GCM();
+        String base64H = H.toBase64GCM();
+        String base64AuthTag = authTag.toBase64GCM();
 
         return ResponseBuilder.multiResponse(Arrays.asList(
                 new Tuple<>("ciphertext", ciphertext),
@@ -55,28 +55,28 @@ public class GCMEncryptAction implements Action {
                 new Tuple<>("H", base64H)));
     }
 
-    public static UBigInt16 calculateAuthKey(String algorithm, UBigInt16 key) {
+    public static FieldElement calculateAuthKey(String algorithm, FieldElement key) {
         return switch (algorithm) {
-            case "aes128" -> new UBigInt16(AES.encrypt(new byte[16], key.toByteArray()), true);
-            case "sea128" -> new UBigInt16(SEA128Action.encryptSEA128(new byte[16], key.toByteArray()), true);
+            case "aes128" -> new FieldElement(AES.encrypt(new byte[16], key.toByteArray()));
+            case "sea128" -> new FieldElement(SEA128Action.encryptSEA128(new byte[16], key.toByteArray()));
             default -> throw new IllegalArgumentException("Algorithm " + algorithm + " not supported");
         };
     }
 
-    public static byte[] generateFullText(String algorithm, String nonce, UBigInt16 key, byte[] text) {
+    public static byte[] generateFullText(String algorithm, String nonce, FieldElement key, byte[] text) {
 
-        List<UBigInt16> ciphertextBlocksList = new ArrayList<>();
+        List<FieldElement> ciphertextBlocksList = new ArrayList<>();
         int blockCount = (text.length / 16) + (text.length % 16 == 0 ? 0 : 1);
 
         for (int i = 0; i < blockCount; i++) {
-            // Copy each 16 byte block into a UBigInt16 (this will auto-pad the last one if
+            // Copy each 16 byte block into a FieldElement (this will auto-pad the last one if
             // needed)
-            UBigInt16 textBlock = new UBigInt16(Arrays.copyOfRange(text, i * 16, (i + 1) * 16), true);
-            UBigInt16 ciphertextBlock = generateSingleTextBlock(i, algorithm, nonce, key, textBlock);
+            FieldElement textBlock = new FieldElement(Arrays.copyOfRange(text, i * 16, (i + 1) * 16));
+            FieldElement ciphertextBlock = generateSingleTextBlock(i, algorithm, nonce, key, textBlock);
             ciphertextBlocksList.add(ciphertextBlock);
         }
 
-        byte[] concatedCiphertext = Util.concatUBigInt16s(ciphertextBlocksList);
+        byte[] concatedCiphertext = Util.concatFieldElements(ciphertextBlocksList);
         // undo the part that resulted from padding the last ciphertext block
         byte[] result = Arrays.copyOfRange(concatedCiphertext, 0, text.length);
 
@@ -84,47 +84,47 @@ public class GCMEncryptAction implements Action {
     }
 
     // also used for creating the Auth Tag
-    public static UBigInt16 generateSingleTextBlock(long index, String algorithm, String nonce, UBigInt16 key,
-            UBigInt16 plaintextPart) {
+    public static FieldElement generateSingleTextBlock(long index, String algorithm, String nonce, FieldElement key,
+            FieldElement plaintextPart) {
 
-        UBigInt16 nonceConcatCounter = concatNonceAndCounter(nonce, index + 2); // the 0th ciphertext block has Ctr 2
+        FieldElement nonceConcatCounter = concatNonceAndCounter(nonce, index + 2); // the 0th ciphertext block has Ctr 2
 
         return switch (algorithm) {
             case "aes128" ->
-                new UBigInt16(AES.encrypt(nonceConcatCounter.toByteArray(), key.toByteArray()), true)
+                new FieldElement(AES.encrypt(nonceConcatCounter.toByteArray(), key.toByteArray()))
                         .xor(plaintextPart);
             case "sea128" ->
-                new UBigInt16(SEA128Action.encryptSEA128(nonceConcatCounter.toByteArray(), key.toByteArray()), true)
+                new FieldElement(SEA128Action.encryptSEA128(nonceConcatCounter.toByteArray(), key.toByteArray()))
                         .xor(plaintextPart);
             default -> throw new IllegalArgumentException("Algorithm " + algorithm + " not supported");
         };
     }
 
-    private static UBigInt16 concatNonceAndCounter(String base64Nonce, long counter) {
+    private static FieldElement concatNonceAndCounter(String base64Nonce, long counter) {
         byte[] ctr = new byte[4]; // counter is assumed to have non-gcm bit order
         ctr[0] = (byte) ((counter >> 24) & 0xFF);
-        ctr[1] = (byte) ((counter >> 16) & 0xFF);
+        ctr[1] = (byte) ((counter >> 16) & 0xFF); //TODO error could lie here
         ctr[2] = (byte) ((counter >> 8) & 0xFF);
         ctr[3] = (byte) (counter & 0xFF);
 
         // 12 byte nonce, rest will be filed with 0s
-        UBigInt16 nonce = UBigInt16.fromBase64(base64Nonce, true);
+        FieldElement nonce = FieldElement.fromBase64GCM(base64Nonce);
         byte[] result = nonce.toByteArray();
 
         // copy counter into remaining 4 bytes
         System.arraycopy(ctr, 0, result, 12, 4);
 
-        return new UBigInt16(result, true);
+        return new FieldElement(result);
     }
 
-    public static UBigInt16 ghash(UBigInt16 authKey, byte[] ad, byte[] ciphertext, UBigInt16 concatedLength) {
-        UBigInt16 lastBlock = UBigInt16.Zero(true); // begin with all 0s
+    public static FieldElement ghash(FieldElement authKey, byte[] ad, byte[] ciphertext, FieldElement concatedLength) {
+        FieldElement lastBlock = FieldElement.Zero(); // begin with all 0s
 
         int adBlockCount = (ad.length / 16) + (ad.length % 16 == 0 ? 0 : 1);
 
         for (int i = 0; i < adBlockCount; i++) {
             int currentMax = (i + 1) * 16 > ad.length ? ad.length : (i + 1) * 16;
-            UBigInt16 currentADBlock = new UBigInt16(Arrays.copyOfRange(ad, i * 16, currentMax), true);
+            FieldElement currentADBlock = new FieldElement(Arrays.copyOfRange(ad, i * 16, currentMax));
             lastBlock = singleGashBlock(lastBlock, currentADBlock, authKey);
         }
 
@@ -132,21 +132,21 @@ public class GCMEncryptAction implements Action {
 
         for (int i = 0; i < ciphertextBlockCount; i++) {
             int currentMax = (i + 1) * 16 > ciphertext.length ? ciphertext.length : (i + 1) * 16;
-            UBigInt16 currentCiphertextBlock = new UBigInt16(Arrays.copyOfRange(ciphertext, i * 16, currentMax), true);
+            FieldElement currentCiphertextBlock = new FieldElement(Arrays.copyOfRange(ciphertext, i * 16, currentMax));
             lastBlock = singleGashBlock(lastBlock, currentCiphertextBlock, authKey);
         }
 
-        UBigInt16 result = singleGashBlock(lastBlock, concatedLength, authKey);
+        FieldElement result = singleGashBlock(lastBlock, concatedLength, authKey);
 
         return result;
     }
 
-    private static UBigInt16 singleGashBlock(UBigInt16 inputA, UBigInt16 inputB, UBigInt16 authKey) {
-        UBigInt16 xor = inputA.xor(inputB);
+    private static FieldElement singleGashBlock(FieldElement inputA, FieldElement inputB, FieldElement authKey) {
+        FieldElement xor = inputA.xor(inputB);
         return GFMulAction.mulAndReduce(xor, authKey);
     }
 
-    public static UBigInt16 calculateLengthOfADAndCiphertexts(byte[] ad, byte[] ciphertext) {
+    public static FieldElement calculateLengthOfADAndCiphertexts(byte[] ad, byte[] ciphertext) {
         // Amount of bits
         long adLengthInt = ad.length * 8;
         long ciphertextLengthInt = ciphertext.length * 8;
@@ -172,7 +172,7 @@ public class GCMEncryptAction implements Action {
             result[i + 8] = ciphertextLength[i];
         }
 
-        return new UBigInt16(result, true);
+        return new FieldElement(result);
     }
 
 }
--- app/src/main/java/me/seyfu_t/model/CipherData.java
+++ app/src/main/java/me/seyfu_t/model/CipherData.java
@@ -7,12 +7,12 @@ import com.google.gson.JsonObject;
 public class CipherData {
     public final byte[] ciphertext;
     public final byte[] ad;
-    public final UBigInt16 tag;
+    public final FieldElement tag;
 
     public CipherData(JsonObject message) {
         this.ciphertext = Base64.getDecoder().decode(getJsonPart(message, "ciphertext"));
         this.ad = Base64.getDecoder().decode(getJsonPart(message, "associated_data"));
-        this.tag = UBigInt16.fromBase64(getJsonPart(message, "tag"), true);
+        this.tag = FieldElement.fromBase64GCM(getJsonPart(message, "tag"));
     }
 
     public byte[] getCiphertext() {
@@ -23,7 +23,7 @@ public class CipherData {
         return this.ad;
     }
 
-    public UBigInt16 getTag() {
+    public FieldElement getTag() {
         return this.tag;
     }
 
