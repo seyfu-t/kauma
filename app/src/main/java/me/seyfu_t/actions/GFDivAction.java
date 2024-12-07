0d069ac9551c4acfb9332006df0e34b11679b21e
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-07 01:37:21 +0100

    implemented gfdiv with FieldElement

--- app/src/main/java/me/seyfu_t/actions/GFDivAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFDivAction.java
@@ -3,6 +3,7 @@ package me.seyfu_t.actions;
 import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
+import me.seyfu_t.model.FieldElement;
 import me.seyfu_t.model.UBigInt16;
 import me.seyfu_t.util.ResponseBuilder;
 
@@ -13,10 +14,32 @@ public class GFDivAction implements Action {
         String base64A = arguments.get("a").getAsString();
         String base64B = arguments.get("b").getAsString();
 
-        UBigInt16 a = UBigInt16.fromBase64(base64A, true);
-        UBigInt16 b = UBigInt16.fromBase64(base64B, true);
+        FieldElement a = FieldElement.fromBase64(base64A);
+        FieldElement b = FieldElement.fromBase64(base64B);
     
         return ResponseBuilder.singleResponse("q", div(a, b).toBase64());
+    }
+
+    public static FieldElement div(FieldElement a, FieldElement b) {
+        return GFMulAction.mulAndReduce(a, inverse(b));
+    }
+
+    public static FieldElement inverse(FieldElement a) {
+        FieldElement result = FieldElement.One(); // 1
+        FieldElement base = a;
+
+        FieldElement pow = FieldElement.AllOne().unsetBit(0); // 2^128 - 2
+
+        while (!pow.isZero()) {
+            if (pow.testBit(0)) // if odd
+                result = GFMulAction.mulAndReduce(result, base);
+
+            base = GFMulAction.mulAndReduce(base, base);
+
+            pow = pow.shiftRight(1); // div by 2
+        }
+
+        return result;
     }
 
     public static UBigInt16 div(UBigInt16 a, UBigInt16 b) {
