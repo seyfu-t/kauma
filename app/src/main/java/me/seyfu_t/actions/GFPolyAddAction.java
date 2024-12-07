ea8bf3f11e70f4ecb9b89d9f0ac32fa0ebd10548
seyfu-t <git@seyfu-t.me>
2024-12-07 02:31:12 +0100

    implemented gfpoly_add with GFPoly and FieldElement classes

--- app/src/main/java/me/seyfu_t/actions/GFPolyAddAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFPolyAddAction.java
@@ -3,7 +3,9 @@ package me.seyfu_t.actions;
 import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
+import me.seyfu_t.model.FieldElement;
 import me.seyfu_t.model.GF128Poly;
+import me.seyfu_t.model.GFPoly;
 import me.seyfu_t.model.UBigInt16;
 import me.seyfu_t.util.ResponseBuilder;
 import me.seyfu_t.util.Util;
@@ -15,10 +17,28 @@ public class GFPolyAddAction implements Action {
         String[] polyA = Util.convertJsonArrayToStringArray(arguments.get("A").getAsJsonArray());
         String[] polyB = Util.convertJsonArrayToStringArray(arguments.get("B").getAsJsonArray());
 
-        GF128Poly a = new GF128Poly(polyA);
-        GF128Poly b = new GF128Poly(polyB);
+        GFPoly a = new GFPoly(polyA);
+        GFPoly b = new GFPoly(polyB);
 
         return ResponseBuilder.singleResponse("S", add(a, b).toBase64Array());
+    }
+
+    public static GFPoly add(GFPoly a, GFPoly b) {
+        int minIndex = Math.min(a.size(), b.size());
+        int maxIndex = Math.max(a.size(), b.size());
+
+        GFPoly result = new GFPoly();
+
+        for (int i = 0; i < minIndex; i++)
+            result.setCoefficient(i, a.getCoefficient(i).xor(b.getCoefficient(i)));
+
+        for (int i = minIndex; i < maxIndex; i++)
+            result.setCoefficient(i, a.size() > b.size() ? a.getCoefficient(i) : b.getCoefficient(i));
+
+        if (result.isZero())
+            result.setCoefficient(0, FieldElement.Zero());
+
+        return result.popLeadingZeros();
     }
 
     public static GF128Poly add(GF128Poly a, GF128Poly b) {
