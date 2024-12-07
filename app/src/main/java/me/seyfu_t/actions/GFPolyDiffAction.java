a444e5fc14085558348da12bb3808a814a36e7e4
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-07 03:00:06 +0100

    implemented gfpoly_mul, gfpoly_diff and gfpoly_pow with new GFPoly and FieldElement classes

--- app/src/main/java/me/seyfu_t/actions/GFPolyDiffAction.java
+++ app/src/main/java/me/seyfu_t/actions/GFPolyDiffAction.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
 import me.seyfu_t.model.GF128Poly;
+import me.seyfu_t.model.GFPoly;
 import me.seyfu_t.model.UBigInt16;
 import me.seyfu_t.util.ResponseBuilder;
 import me.seyfu_t.util.Util;
@@ -14,9 +15,28 @@ public class GFPolyDiffAction implements Action {
     public JsonObject execute(JsonObject arguments) {
         String[] poly = Util.convertJsonArrayToStringArray(arguments.get("F").getAsJsonArray());
 
-        GF128Poly q = new GF128Poly(poly);
+        GFPoly q = new GFPoly(poly);
 
         return ResponseBuilder.singleResponse("F'", diff(q).toBase64Array());
+    }
+
+    public static GFPoly diff(GFPoly poly) {
+        if (poly.isZero())
+            return GFPoly.ZERO_POLY;
+
+        GFPoly result = new GFPoly();
+
+        // Skip first one, effectively reduce all exponents by 1
+        for (int i = 1; i < poly.size(); i++)
+            if (i % 2 == 1)
+                result.setCoefficient(i - 1, poly.getCoefficient(i));
+
+        // result = result.popLeadingZeros();
+
+        if (result.isZero())
+            return GFPoly.ZERO_POLY;
+
+        return result;
     }
 
     public static GF128Poly diff(GF128Poly poly) {
