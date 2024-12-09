7868728210573712775b612f708a3031e1308144
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-09 16:57:05 +0100

    simplification and reduction of conversions

--- app/src/main/java/me/seyfu_t/actions/XEXAction.java
+++ app/src/main/java/me/seyfu_t/actions/XEXAction.java
@@ -1,37 +1,29 @@
 package me.seyfu_t.actions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.List;
 
 import com.google.gson.JsonObject;
 
 import me.seyfu_t.model.Action;
 import me.seyfu_t.model.FieldElement;
 import me.seyfu_t.util.ResponseBuilder;
-import me.seyfu_t.util.Util;
 
 public class XEXAction implements Action {
 
     @Override
     public JsonObject execute(JsonObject arguments) {
         String mode = arguments.get("mode").getAsString();
-        String key = arguments.get("key").getAsString();
-        String tweak = arguments.get("tweak").getAsString();
-        String input = arguments.get("input").getAsString();
+        byte[] key = Base64.getDecoder().decode(arguments.get("key").getAsString());
+        byte[] tweak = Base64.getDecoder().decode(arguments.get("tweak").getAsString());
+        byte[] input = Base64.getDecoder().decode(arguments.get("input").getAsString());
 
         return ResponseBuilder.singleResponse("output", xex(mode, key, tweak, input));
     }
 
-    private static String xex(String mode, String base64Key, String base64Tweak, String base64Input) {
-        byte[] fullKey = Base64.getDecoder().decode(base64Key);
-        FieldElement keyOne = new FieldElement(Arrays.copyOfRange(fullKey, 0, fullKey.length / 2));
-        FieldElement keyTwo = new FieldElement(Arrays.copyOfRange(fullKey, fullKey.length / 2, fullKey.length));
-
-        FieldElement tweak = FieldElement.fromBase64XEX(base64Tweak);
-
-        byte[] input = Base64.getDecoder().decode(base64Input);
+    private static String xex(String mode, byte[] key, byte[] tweak, byte[] input) {
+        byte[] keyOne = Arrays.copyOfRange(key, 0, key.length / 2);
+        byte[] keyTwo = Arrays.copyOfRange(key, key.length / 2, key.length);
 
         byte[] output = switch (mode) {
             case "encrypt" -> cryptAllBlocks("encrypt", input, tweak, keyOne, keyTwo);
@@ -39,49 +31,47 @@ public class XEXAction implements Action {
             default -> throw new IllegalArgumentException("Null is not a valid mode");
         };
 
-        String out = Base64.getEncoder().encodeToString(output);
-        return out;
+        return Base64.getEncoder().encodeToString(output);
     }
 
-    private static byte[] cryptAllBlocks(String mode, byte[] input, FieldElement tweak, FieldElement keyOne,
-            FieldElement keyTwo) {
-        List<FieldElement> blocksList = new ArrayList<>();
+    private static byte[] cryptAllBlocks(String mode, byte[] input, byte[] tweak, byte[] keyOne, byte[] keyTwo) {
+        byte[] output = new byte[input.length];
 
         for (int i = 0; i < input.length / 16; i++) {
-            FieldElement roundPlainOrCipherText = new FieldElement(Arrays.copyOfRange(input, i * 16, (i + 1) * 16));
+            byte[] roundPlainOrCipherText = Arrays.copyOfRange(input, i * 16, (i + 1) * 16);
 
             FieldElement roundKey = getMasterKeyForRound(i, tweak, keyTwo);
 
-            FieldElement block = cryptSingleBlock(mode, roundPlainOrCipherText, roundKey, keyOne);
-            blocksList.add(block);
+            byte[] block = cryptSingleBlock(mode, roundPlainOrCipherText, roundKey.toByteArrayXEX(), keyOne);
+            System.arraycopy(block, 0, output, i * 16, 16);
         }
-        return Util.concatFieldElementsXEX(blocksList);
+        return output;
     }
 
-    private static FieldElement getMasterKeyForRound(int round, FieldElement tweak, FieldElement keyTwo) {
+    private static FieldElement getMasterKeyForRound(int round, byte[] tweak, byte[] keyTwo) {
         // Starter key
-        String base64MasterKey = SEA128Action.sea128("encrypt", tweak.toBase64XEX(), keyTwo.toBase64XEX());
+        FieldElement masterKey = new FieldElement(SEA128Action.encryptSEA128(tweak, keyTwo));
 
         // For every other block, multiply with alpha in GF2^128
-        for (int i = 0; i < round; i++) {
-            FieldElement roundKey = GFMulAction.mulAndReduce(FieldElement.fromBase64XEX(base64MasterKey), FieldElement.ALPHA);
-            base64MasterKey = roundKey.toBase64XEX();
-        }
+        for (int i = 0; i < round; i++)
+            masterKey = GFMulAction.mulAndReduce(masterKey, FieldElement.ALPHA);
 
-        return FieldElement.fromBase64XEX(base64MasterKey);
+        return masterKey;
     }
 
-    private static FieldElement cryptSingleBlock(String mode, FieldElement text, FieldElement roundKey, FieldElement keyOne) {
+    private static byte[] cryptSingleBlock(String mode, byte[] text, byte[] roundKey, byte[] keyOne) {
         // XOR
-        FieldElement block = text.xor(roundKey);
+        for (int i = 0; i < 16; i++)
+            text[i] ^= roundKey[i];
 
         // Encrypt or Decrypt
-        block = FieldElement.fromBase64XEX(SEA128Action.sea128(mode, block.toBase64XEX(), keyOne.toBase64XEX()));
+        text = SEA128Action.sea128(mode, text, keyOne);
 
         // XOR
-        block = block.xor(roundKey);
+        for (int i = 0; i < 16; i++)
+            text[i] ^= roundKey[i];
 
-        return block;
+        return text;
     }
 
 }
