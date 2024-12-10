83231026d9ab4ab26e99d398b5ceae6bc0338521
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-10 16:04:58 +0100

    adjusted comment to use javadoc syntax

--- app/src/main/java/me/seyfu_t/model/Action.java
+++ app/src/main/java/me/seyfu_t/model/Action.java
@@ -3,8 +3,8 @@ package me.seyfu_t.model;
 import com.google.gson.JsonObject;
 
 public interface Action {
-    /*
-     * @param takes in the contents of "arguments":{}
+    /**
+     * @param arguments takes in the contents of "arguments":{}
      * 
      * @return action result which is a key-value pair with the key being the name
      * of the output (e.g. "sum" or "block") and the value being the result data
