f27174839c91db5669880038f78a181514d6a866
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-10 01:46:12 +0100

    clear AppTest from any unit tests, keep full file tests here

--- app/src/test/java/me/seyfu_t/AppTest.java
+++ app/src/test/java/me/seyfu_t/AppTest.java
@@ -1,8 +1,5 @@
 package me.seyfu_t;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,9 +7,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.google.gson.JsonObject;
-
-import me.seyfu_t.model.FieldElement;
-import me.seyfu_t.util.Util;
 
 class AppTest {
 
@@ -75,116 +69,6 @@ class AppTest {
 
     private static final String ALL_INPUT = "input/AllInput.json";
     private static final String ALL_OUTPUT = "output/AllOutput.json";
-
-    // @Test
-    // void testConcatMultiples(){
-    //     UBigInt16 a1 = new UBigInt16().setBit(0).setBit(5);
-    //     UBigInt16 b1 = new UBigInt16().setBit(3).setBit(8);
-
-    //     List<UBigInt16> list1 = new ArrayList<>();
-    //     list1.add(a1);
-    //     list1.add(b1);
-
-    //     byte[] concatted1 = Util.concatUBigInt16s(list1);
-
-
-    //     FieldElement a2 = new FieldElement().setBit(0).setBit(5);
-    //     FieldElement b2 = new FieldElement().setBit(3).setBit(8);
-
-    //     List<FieldElement> list2 = new ArrayList<>();
-    //     list2.add(a2);
-    //     list2.add(b2);
-
-    //     byte[] concatted2 = Util.concatFieldElementsXEX(list2);
-
-    //     System.err.println(Arrays.toString(concatted1));
-    //     System.err.println(Arrays.toString(concatted2));
-
-    //     Assertions.assertArrayEquals(concatted1, concatted2);
-    // }
-
-    // @Test
-    // void testByteArrayBitShift() {
-    //     byte[] tb = new byte[] { (byte) 0x81, (byte) 0xFF, (byte) 0x18, (byte) 0x4c,
-    //             (byte) 0x81, (byte) 0x81, (byte) 0xCC, (byte) 0x4c,
-    //             (byte) 0x81, (byte) 0x81, (byte) 0x18, (byte) 0x4c,
-    //             (byte) 0x81, (byte) 0x81, (byte) 0x18, (byte) 0x4c, };
-    //     UBigInt16 b = new UBigInt16(tb);
-
-    //     String shift1Expectation = "10011000 00110001 00000011 00000010 10011000 00110001 00000011 00000010 10011001 10011001 00000011 00000010 10011000 00110001 11111111 00000010";
-    //     String shift2Expectation = "00110000 01100010 00000110 00000101 00110000 01100010 00000110 00000101 00110011 00110010 00000110 00000101 00110000 01100011 11111110 00000100";
-    //     String shift3Expectation = "01100000 11000100 00001100 00001010 01100000 11000100 00001100 00001010 01100110 01100100 00001100 00001010 01100000 11000111 11111100 00001000";
-
-    //     String shift1Reality = b.shiftLeft(1).swapEndianness().toString(2);
-    //     String shift2Reality = b.shiftLeft(2).swapEndianness().toString(2);
-    //     String shift3Reality = b.shiftLeft(3).swapEndianness().toString(2);
-
-    //     String shift4Expectation = "00000010 11111111 00110001 10011000 00000010 00000011 10011001 10011001 00000010 00000011 00110001 10011000 00000010 00000011 00110001 10011000";
-    //     String shift5Expectation = "00000100 11111110 01100011 00110000 00000101 00000110 00110010 00110011 00000101 00000110 01100010 00110000 00000101 00000110 01100010 00110000";
-    //     String shift6Expectation = "00001000 11111100 11000111 01100000 00001010 00001100 01100100 01100110 00001010 00001100 11000100 01100000 00001010 00001100 11000100 01100000";
-
-    //     String shift4Reality = b.shiftLeft(1).toString(2);
-    //     String shift5Reality = b.shiftLeft(2).toString(2);
-    //     String shift6Reality = b.shiftLeft(3).toString(2);
-
-    //     Assertions.assertEquals(shift1Expectation, shift1Reality);
-    //     Assertions.assertEquals(shift2Expectation, shift2Reality);
-    //     Assertions.assertEquals(shift3Expectation, shift3Reality);
-
-    //     Assertions.assertEquals(shift4Expectation, shift4Reality);
-    //     Assertions.assertEquals(shift5Expectation, shift5Reality);
-    //     Assertions.assertEquals(shift6Expectation, shift6Reality);
-
-    // }
-
-    // @Test
-    // void testComparators() {
-    //     UBigInt16[] aa = new UBigInt16[] {
-    //             UBigInt16.Zero(), UBigInt16.Zero(), UBigInt16.Zero(),
-    //     };
-    //     UBigInt16[] bb = new UBigInt16[] {
-    //             UBigInt16.Zero()
-    //     };
-    //     UBigInt16[] cc = new UBigInt16[] {
-    //             UBigInt16.AllOne(),
-    //     };
-    //     UBigInt16[] dd = new UBigInt16[] {
-    //             new UBigInt16(new byte[] { (byte) 0xAF })
-    //     };
-    //     UBigInt16[] ee = new UBigInt16[] {
-    //             new UBigInt16(new byte[] { (byte) 0x01 })
-    //     };
-    //     UBigInt16[] ff = new UBigInt16[] {
-    //             new UBigInt16(new byte[] { (byte) 0xFF })
-    //     };
-
-    //     GF128Poly a = new GF128Poly(aa);
-    //     GF128Poly b = new GF128Poly(bb);
-    //     GF128Poly c = new GF128Poly(cc);
-    //     GF128Poly d = new GF128Poly(dd);
-    //     GF128Poly e = new GF128Poly(ee);
-    //     GF128Poly f = new GF128Poly(ff);
-
-    //     // Equality checks
-    //     Assertions.assertTrue(a.equals(b));
-    //     Assertions.assertTrue(b.equals(a));
-    //     Assertions.assertFalse(a.equals(c));
-
-    //     // Greater than checks
-    //     Assertions.assertTrue(c.greaterThan(d)); // AllOne > 0xAF
-    //     Assertions.assertTrue(d.greaterThan(e)); // 0xAF > 0x01
-    //     Assertions.assertFalse(f.greaterThan(c)); // 0xFF > AllOne
-
-    //     // Less than checks
-    //     Assertions.assertTrue(d.lessThan(c)); // 0xAF < AllOne
-    //     Assertions.assertTrue(e.lessThan(d)); // 0x01 < 0xAF
-    //     Assertions.assertFalse(c.lessThan(f)); // AllOne < 0xFF
-
-    //     // Mixed relational checks
-    //     Assertions.assertTrue(a.lessThan(e)); // Zero < 0x01
-    //     Assertions.assertFalse(a.greaterThan(b)); // Zero is not greater than Zero
-    //     Assertions.assertTrue(f.greaterThan(b)); // 0xFF > Zero
-    // }
 
     @Test
     void paddingOracle() {
