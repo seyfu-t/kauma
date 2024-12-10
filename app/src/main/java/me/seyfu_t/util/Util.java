00b1e51604d3b316b564d2f2c449ce5f23e3aba7
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-10 16:23:31 +0100

    implemented miller-rabin test

--- app/src/main/java/me/seyfu_t/util/Util.java
+++ app/src/main/java/me/seyfu_t/util/Util.java
@@ -131,4 +131,73 @@ public class Util {
         return byteArray;
     }
 
+    // Miller-Rabin test
+    public boolean isPrime(long candidate, long rounds) {
+        long d, s;
+
+        if (candidate == 2)
+            return true;
+        if (candidate < 2)
+            return false;
+
+        // until d is odd
+        for (d = 0, s = 1; (d & 1) == 0; s++)
+            d = (candidate - 1) / pow(2, s);
+
+        verification: for (long i = 0; i < rounds; i++) {
+            // random base in the range [2, n-1]
+            long base = (long) ((Math.random() * (candidate - 3)) + 2);
+
+            long x = powMod(base, d, candidate);
+
+            if (x == 1 || x == (candidate - 1))
+                continue verification;
+
+            for (long j = 0; j < (s - 1); j++) {
+                x = powMod(x, 2, candidate);
+                if (x == 1)
+                    return false;
+                if (x == (candidate - 1))
+                    continue verification;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static long pow(long base, long exponent) {
+        int shift = 63; // bit position
+        long result = base;
+
+        // Skip all leading 0 bits and the most significant 1 bit.
+        while (((exponent >> shift--) & 1) == 0)
+            ;
+
+        while (shift >= 0) {
+            result = result * result;
+            if (((exponent >> shift--) & 1) == 1)
+                result = result * base;
+        }
+
+        return result;
+    }
+
+    public static long powMod(long base, long exponent, long modulo) {
+        int shift = 63; // bit position
+        long result = base;
+
+        // Skip all leading 0 bits and the most significant 1 bit.
+        while (((exponent >> shift--) & 1) == 0)
+            ;
+
+        while (shift >= 0) {
+            result = (result * result) % modulo;
+            if (((exponent >> shift--) & 1) == 1)
+                result = (result * base) % modulo;
+        }
+
+        return result;
+    }
 }
