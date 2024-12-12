f31cd4dd2290148202a49e1568a4369f46bafc88
seyfu-t <seyfullahtalay@hotmail.com>
2024-12-12 01:36:06 +0100

    correctly implemented glasskey_prng

--- app/src/main/java/me/seyfu_t/actions/glasskey/GlasskeyPRNGAction.java
+++ app/src/main/java/me/seyfu_t/actions/glasskey/GlasskeyPRNGAction.java
@@ -12,60 +12,47 @@ import me.seyfu_t.util.Util;
 
 public class GlasskeyPRNGAction implements Action {
 
+    private static final int BLOCK_SIZE = 32;
+
     @Override
     public JsonObject execute(JsonObject arguments) {
         byte[] key = Base64.getDecoder().decode(arguments.get("agency_key").getAsString());
         byte[] seed = Base64.getDecoder().decode(arguments.get("seed").getAsString());
-        long[] lengths = Util.convertJsonArrayToLongArray(arguments.get("lengths").getAsJsonArray());
+        int[] lengths = Util.convertJsonArrayToIntegerArray(arguments.get("lengths").getAsJsonArray());
 
         return ResponseBuilder.singleResponse("blocks", prngAll(key, seed, lengths));
     }
 
-    public static String[] prngAll(byte[] key, byte[] seed, long[] nums) {
+    public static String[] prngAll(byte[] key, byte[] seed, int[] nums) {
         String[] result = new String[nums.length];
 
+        byte[] buffer = new byte[nums.length * BLOCK_SIZE];
+
         for (int i = 0; i < nums.length; i++) {
-            byte[] block = Arrays.copyOfRange(prngSingle(key, seed, nums[i]), 0, (int) nums[i]);
-            result[i] = Base64.getEncoder().encodeToString(block);
+            byte[] block = prngSingle(key, seed, i);
+            System.arraycopy(block, 0, buffer, i * BLOCK_SIZE, block.length);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = Base64.getEncoder().encodeToString(Arrays.copyOfRange(buffer, 0, nums[i]));
+            buffer = Arrays.copyOfRange(buffer, nums[i], buffer.length);
         }
 
         return result;
 
     }
 
-    public static byte[] prngSingle(byte[] key, byte[] seed, long num) {
-        byte[] numStar = Util.longToBytesLittleEndian(num);
+    public static byte[] prngSingle(byte[] key, byte[] seed, int num) {
+        byte[] numStar = Util.intToBytesLittleEndian(num);
 
-        byte[] keySha = Util.swapByteOrder(SHA.sha256(key));
-        byte[] seedSha = Util.swapByteOrder(SHA.sha256(seed));
+        byte[] keySha = SHA.sha256(key);
+        byte[] seedSha = SHA.sha256(seed);
 
-        byte[] keyStar = new byte[32];
-        System.arraycopy(keySha, 0, keyStar, 0, 16);
-        System.arraycopy(seedSha, 0, keyStar, 16, 16);
+        byte[] keyStar = new byte[64];
+        System.arraycopy(keySha, 0, keyStar, 0, BLOCK_SIZE);
+        System.arraycopy(seedSha, 0, keyStar, BLOCK_SIZE, BLOCK_SIZE);
 
-        byte[] hmac = Util.swapByteOrder(SHA.hmacSha256(numStar, keyStar));
-        hmac = Util.swapByteOrder(hmac);
-        System.out.println("KEY: " + toHex(keySha));
-        System.out.println("SEED: " + toHex(seedSha));
-        System.out.println("HMAC: " + toHex(hmac));
-
-        return hmac;
-    }
-
-    public static String toHex(byte[] input) {
-        if (input.length == 0)
-            return "0";
-
-        StringBuilder hex = new StringBuilder();
-        for (int i = input.length - 1; i >= 0; i--)
-            hex.append(String.format("%02x", input[i]));
-
-        while (hex.charAt(0) == '0' && hex.length() > 1)
-            hex.deleteCharAt(0);
-
-        hex.deleteCharAt(hex.length() - 1); // remove final space
-
-        return hex.toString().toUpperCase();
+        return SHA.hmacSha256(numStar, keyStar);
     }
 
 }
